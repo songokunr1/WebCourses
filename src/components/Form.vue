fecfc931fd2e7fd97f66580f8c63851f58f5531a
songokunr1 <pawelm18@gmail.com>
2023-05-27 16:11:07 +0200

    1.0.3 docker network implementation form correction and /thankyou route working.

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,184 +1,197 @@
 <template>
-  <div class="card">
+    <div class="card">
     <v-card class="rounded-card">
       <v-card-text>
-        <form @submit.prevent="submit">
-          <v-text-field
-            v-model="name.value.value"
-            :counter="3"
-            maxlength="15"
-            :error-messages="name.errorMessage.value"
-            label="Imię"
-          ></v-text-field>
+    <form @submit.prevent="submit">
+      <v-text-field
+        v-model="name.value.value"
+        :counter="3"
+        maxlength="15"
+        :error-messages="name.errorMessage.value"
+        label="Imię"
+      ></v-text-field>
 
-          <v-text-field
-            v-model="surname.value.value"
-            :counter="3"
-            maxlength="15"
-            :error-messages="surname.errorMessage.value"
-            label="Nazwisko"
-          ></v-text-field>
+      <v-text-field
+        v-model="surname.value.value"
+        :counter="3"
+        maxlength="15"
+        :error-messages="surname.errorMessage.value"
+        label="Nazwisko"
+      ></v-text-field>
 
-          <v-text-field
-            v-model="phone.value.value"
-            :counter="9"
-            maxlength="9"
-            :error-messages="phone.errorMessage.value"
-            label="Phone Number"
-          ></v-text-field>
+      <v-text-field
+        v-model="phone.value.value"
+        :counter="9"
+        maxlength="9"
+        :error-messages="phone.errorMessage.value"
+        label="Phone Number"
+      ></v-text-field>
 
-          <v-text-field
-            v-model="email.value.value"
-            :error-messages="email.errorMessage.value"
-            label="E-mail"
-          ></v-text-field>
+      <v-text-field
+        v-model="email.value.value"
+        :error-messages="email.errorMessage.value"
+        label="E-mail"
+      ></v-text-field>
 
-          <v-select
-            v-model="topic.value.value"
-            :items="items"
-            :error-messages="topic.errorMessage.value"
-            label="Temat"
-          ></v-select>
+      <v-select
+        v-model="topic.value.value"
+        :items="items"
+        :error-messages="topic.errorMessage.value"
+        label="Temat"
+      ></v-select>
 
-          <v-textarea
-            v-model="text.value.value"
-            :counter="150"
-            maxlength="150"
-            :error-messages="text.errorMessage.value"
-            label="Wiadomość"
-          ></v-textarea>
+      <v-textarea
+        v-model="text.value.value"
+        :counter="150"
+        maxlength="150"
+        :error-messages="text.errorMessage.value"
+        label="Wiadomość"
+      ></v-textarea>
 
-          <v-checkbox
-            v-model="checkbox.value.value"
-            :error-messages="checkbox.errorMessage.value"
-            :true-value="1"
-            :false-value="0"
-            label="Zgadzam się na przetwarzanie danych w celu nawiązania kontaktu i/lub przedstawieniu oferty"
-            type="checkbox"
-          ></v-checkbox>
+      <v-checkbox
+        v-model="checkbox.value.value"
+        :error-messages="checkbox.errorMessage.value"
+        value="1"
+        label="Zgadzam się na przetwarzanie danych w celu nawiązania kontaktu i/lub przedstawieniu oferty"
+        type="checkbox"
 
-          <v-btn class="me-4" type="submit">Submit</v-btn>
+      ></v-checkbox>
 
-          <v-btn @click="handleReset">Clear</v-btn>
-        </form>
-      </v-card-text>
+      <v-btn
+        class="me-4"
+        type="submit"
+      >
+        submit
+      </v-btn>
+
+      <v-btn @click="handleReset">
+        clear
+      </v-btn>
+    </form>
+       </v-card-text>
     </v-card>
-  </div>
-  <br><br><br><br>
-</template>
-<script>
-import {ref} from 'vue'
-import {useField, useForm} from 'vee-validate'
-import axios from 'axios';
+</div>
+<br><br><br><br>
+  </template>
+  <script>
+    import { ref } from 'vue'
+    import { useField, useForm } from 'vee-validate'
+    import axios from 'axios';
+    import { useRouter } from 'vue-router';
 
-export default {
-  setup() {
-    const {handleSubmit, handleReset} = useForm({
-      validationSchema: {
-        name(value) {
-          if (value?.length >= 2) return true
+    export default {
+      setup () {
+        const router = useRouter();
 
-          return 'Name needs to be at least 2 characters.'
-        },
-        phone(value) {
-          if (value?.length >= 9 && /[0-9-]+/.test(value)) return true
+        const { handleSubmit, handleReset } = useForm({
+          validationSchema: {
+            name (value) {
+              if (value?.length >= 2) return true
 
-          return 'Phone number needs to be at least 9 digits.'
-        },
-        email(value) {
-          if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value)) return true
+              return 'Name needs to be at least 2 characters.'
+            },
+            phone (value) {
+              if (value?.length >= 9 && /[0-9-]+/.test(value)) return true
 
-          return 'Must be a valid e-mail.'
-        },
-        topic(value) {
-          if (value) return true
+              return 'Phone number needs to be at least 9 digits.'
+            },
+            email (value) {
+              if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value)) return true
 
-          return 'Select an item.'
-        },
-        checkbox(value) {
-          if (value === '1') return true
+              return 'Must be a valid e-mail.'
+            },
+            topic (value) {
+              if (value) return true
 
-          return 'Must be checked.'
-        },
-        text(value) {
-          if (value?.length <= 350) return true
+              return 'Select an item.'
+            },
+            checkbox (value) {
+              if (value === '1') return true
 
-          return 'Max 150 chars'
-        },
-      },
-    })
-    const name = useField('name')
-    const surname = useField('surname')
-    const phone = useField('phone')
-    const email = useField('email')
-    const text = useField('text')
+              return 'Must be checked.'
+            },
+            text (value) {
+              if (value?.length <= 350 ) return true
 
-    const topic = useField('topic')
-    const checkbox = useField('checkbox')
-    checkbox.value.value = '1'
+              return 'Max 150 chars'
+            },
+          },
+        })
+        const name = useField('name')
+        const surname = useField('surname')
+        const phone = useField('phone')
+        const email = useField('email')
+        const text = useField('text')
 
-    const items = ref([
-      'Korepetycje',
-      'Python od Podstaw',
-      'Python Programowanie Obiektowe',
-      'Python wstęp do Analizy Danych',
+        const topic = useField('topic')
+        const checkbox = useField('checkbox')
+        checkbox.value.value = '1'
 
-      // 'Python testing',
-      // 'Python wstęp do Big Data',
-      'Python Machine Learning',
-      // 'Databricks Exploration',
-      // 'Python wstęp do Data Enginier'
-      'Inny',
-    ])
+        const items = ref([
+          'Korepetycje',
+          'Python od Podstaw',
+          'Python Programowanie Obiektowe',
+          'Python wstęp do Analizy Danych',
 
-    //       const submit = handleSubmit(async values => {
-    // try {
-    //   const response = await axios.post('http://127.0.0.1:8888/send_email', JSON(values)
-    //   );}
-    //   catch (error) {
-    //       // Handle any errors that occur during the API request
-    //       console.error(error);
-    //     }
-    //     })
+          // 'Python testing',
+          // 'Python wstęp do Big Data',
+          'Python Machine Learning',
+          // 'Databricks Exploration',
+          // 'Python wstęp do Data Enginier'
+          'Inny',
+        ])
+
+  //       const submit = handleSubmit(async values => {
+  // try {
+  //   const response = await axios.post('http://127.0.0.1:8888/send_email', JSON(values)
+  //   );}
+  //   catch (error) {
+  //       // Handle any errors that occur during the API request
+  //       console.error(error);
+  //     }
+  //     })
+
 
 
     const submit = handleSubmit(async values => {
-      try {
-        const response = await axios.post('http://127.0.0.1:8888/send_email', {
-          name: values.name,
-          surname: values.surname,
-          phone: values.phone,
-          email: values.email,
-          topic: values.topic,
-          text: values.text
-        });
-        // Handle the response from the API as needed
-        console.log(response.data);
+  try {
+    const response = await axios.post('http://127.0.0.1:8888/send_email', {
+      name: values.name,
+      surname: values.surname,
+      phone: values.phone,
+      email: values.email,
+      topic: values.topic,
+      text: values.text
+      });
+      // Handle the response from the API as needed
+      console.log(response.data);
       } catch (error) {
         // Handle any errors that occur during the API request
         console.error(error);
       }
-      try {
-        const response = await axios.post('http://127.0.0.1:8889/save_form_to_db', {
-          name: values.name,
-          surname: values.surname,
-          phone: values.phone,
-          email: values.email,
-          topic: values.topic,
-          text: values.text
-        });
-        // Handle the response from the API as needed
-        console.log(response.data);
-      } catch (error) {
-        // Handle any errors that occur during the API request
-        console.error(error);
-      }
-    })
+    //   try {
+    // const response = await axios.post('http://127.0.0.1:8889/save_form_to_db', {
+    //   name: values.name,
+    //   surname: values.surname,
+    //   phone: values.phone,
+    //   email: values.email,
+    //   topic: values.topic,
+    //   text: values.text
+    //   });
+    //   // Handle the response from the API as needed
+    //   console.log(response.data);
+    //   } catch (error) {
+    //     // Handle any errors that occur during the API request
+    //     console.error(error);
+    //   }
+      router.push('/thankyou');
+      })
 
-    return {name, surname, phone, email, topic, text, checkbox, items, submit, handleReset}
-  },
-}
-</script>
+        return { name, surname, phone, email, topic, text, checkbox, items, submit, handleReset }
+      },
+    }
+  </script>
+
 <style scoped>
 .card {
   width: 100%;
